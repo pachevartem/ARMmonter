1fdd2ea5cacd8d2ab5bafc1f61f7731ed472ce5f
pachevArtem <pachevartem@ya.ru>
2016-08-17 16:43:32 +0300

    move component

---
+++ Assets/_scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using JetBrains.Annotations;
+
+namespace MonterProject
+{
+
+
+    public class GameController : MonoBehaviour
+    {
+        public GameObject Player;
+        public iTweenPath Path;
+        private Animator _animator;
+        public int TimeRun;
+        private void Start()
+        {
+            _animator = Player.GetComponent<Animator>();
+            _animator.SetTrigger("run");
+            iTween.MoveTo(Player, iTween.Hash(
+                "path", iTweenPath.GetPath("Artem"),
+                "time", TimeRun, 
+                "easetype",iTween.EaseType.linear,
+                "orienttopath", true
+                ));
+
+
+        }
+    }
+}
